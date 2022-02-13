fb9a7ef94413fe9ba2bb67a0005197fb651b9297
김준영 <rlawnsdud05@douzone.com>
2022-02-13 13:43:49 +0900

    #섬 연결하기 - 주석 추가

--- graph/섬연결하기_김준영.js
+++ graph/섬연결하기_김준영.js
@@ -1,7 +1,9 @@
 const costs = [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]];
 const n = 4;
+
 const solution = (n, costs) => {
-    //결과값 = 간선들의 가중치 합
+
+    //간선들의 가중치 합
     let totalBridgeCost = 0;
 
     //# MST 구현하기
@@ -14,31 +16,33 @@ const solution = (n, costs) => {
     //1) 간선들의 가중치 오름차순으로 정렬
     const costsByIncrease = costs.sort((a,b) => a[2] - b[2]);
 
-    const root = [];
-
+    //2) 사이클을 형성하는 간선을 제외하기 한다.
+    //사이클 여부 알아보는 법 Union-find 알고리즘
+    //2-1. 자기 자신이 root인 node(섬)를 만든다.
+    const parent = [];
     for(let i = 0; i < n; i ++){
-        root[i] = i;
+        parent[i] = i;
     }
 
-    for(let eachCost of costsByIncrease){
+    for(let eachInfo of costsByIncrease){
 
-        const node1 = eachCost[0];
-        const node2 = eachCost[1];
-        const cost = eachCost[2];
+        const node1 = eachInfo[0];
+        const node2 = eachInfo[1];
+        const cost = eachInfo[2];
 
         //2) 간선들 연결(단, 사이클이 형성되는 간선 제외)
         union(node1, node2, cost);
     }
 
-    function union (x, y, cost) {
+    function union (node1, node2, cost) {
 
-        const xRoot = findRoot(x);
-        const yRoot = findRoot(y);
+        const node1Root = findRoot(node1);
+        const node2Root = findRoot(node2);
 
         //연결 시 사이클 형성되는 간선 제외 조건 추가
-        //rootNode가 다른 경우
-        if(xRoot !== yRoot){
-            (root[y] = xRoot);
+        //연결하려는 node(섬)의 rootNode가 다른 경우에만 다리를 건설한다.(같으면 이미 연결되어 있다는 의미)
+        if(node1Root !== node2Root){
+            (parent[node2] = node1);
             totalBridgeCost += cost;
         }
     }
@@ -46,7 +50,7 @@ const solution = (n, costs) => {
     //rootNode를 찾는 함수
     //parent가 자기 자신인 경우가 rootNode
     function findRoot (x) {
-        return root[x] === x ? x : root[x] = findRoot(root[x]);
+        return parent[x] === x ? x : findRoot(parent[x]);
     }
 
     return totalBridgeCost;
